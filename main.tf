b8a4a07801f06a16b626ef7e91633f0538b2925f
snehalbelli <112497261+snehalbelli@users.noreply.github.com>
2023-02-23 22:02:48 +0530

    3rd commit

--- main.tf
+++ main.tf
@@ -1,30 +1,13 @@
 provider "aws" {
-  region = "us-east-2"
+  region  = "us-east-2"
 }
 
-resource "aws_instance" "ec2" {
-  ami ="ami-0ab0629dba5ae551d"
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
   instance_type = "t2.micro"
-  key_name = "terraform"
-  
- tags ={
-    Name="EC2Terraform"
-    Batch ="5am"
- }
-}
 
-resource "aws_security_group" "security" {
-  
-  name = "new_sg"
-  description = "new_sg"
-
-  ingress {
-    protocol = "tcp"
-    from_port = "22"
-    to_port = "22"
-    cidr_blocks = [ "0.0.0.0/0" ]
+  tags = {
+    Name = "ExampleAppServerInstance"
   }
-  lifecycle {
-    create_before_destroy = true
 }
-}
+
