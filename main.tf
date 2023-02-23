663046b234f89867252d4eb36d7c18d01f9e9a5f
snehalbelli <112497261+snehalbelli@users.noreply.github.com>
2023-02-23 18:39:39 +0530

    1st commit

---
+++ main.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  region = "us-east-2"
+  access_key = "AKIA4JO5BYXWSOLPXAKL"
+  secret_key = "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"
+}
+
+resource "aws_instance" "ec2" {
+  ami ="ami-00eeedc4036573771"
+  instance_type = "t2.micro"
+  key_name = "terraform"
+  
+ tags ={
+    Name="EC2Terraform"
+    Batch ="5am"
+ }
+}
+
+resource "aws_security_group" "security" {
+  
+  name = "new_sg"
+  description = "new_sg"
+
+  ingress {
+    protocol = "tcp"
+    from_port = "22"
+    to_port = "22"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+  lifecycle {
+    create_before_destroy = true
+}
+}
